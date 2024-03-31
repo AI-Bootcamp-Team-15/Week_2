bfb070ddd7c90657e57241c78f82a3d20e8f2ee9
Donald Emeka Achugo <donald@bcbgroup.io>
2024-03-31 23:55:20 +0100

    Update code for AI Jokes Generator interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,154 @@
-'use client';
- 
-import { useChat } from 'ai/react';
- 
+"use client";
+
+import { useState } from "react";
+import { useChat } from "ai/react";
+
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, append, isLoading } = useChat();
+  const topics = [
+    { emoji: "😂", value: "Humor" },
+    { emoji: "🎭", value: "Comedy" },
+    { emoji: "🤡", value: "Satire" },
+    { emoji: "💼", value: "Work" },
+    { emoji: "👥", value: "People" },
+    { emoji: "🐾", value: "Animals" },
+    { emoji: "🍔", value: "Food" },
+    { emoji: "📺", value: "Television" },
+  ];
+  const tones = [
+    { emoji: "😊", value: "Happy" },
+    { emoji: "😢", value: "Sad" },
+    { emoji: "😂", value: "Funny" },
+    { emoji: "🎩", value: "Witty" },
+    { emoji: "🃏", value: "Sarcastic" },
+    { emoji: "😜", value: "Silly" },
+    { emoji: "😈", value: "Dark" },
+    { emoji: "🤪", value: "Goofy" },
+  ];
+  const types = [
+    { emoji: "🔤", value: "Pun" },
+    { emoji: "🚪", value: "Knock-Knock" },
+    { emoji: "📜", value: "One-liner" },
+    { emoji: "❓", value: "Riddle" },
+    { emoji: "🔠", value: "Wordplay" },
+    { emoji: "👀", value: "Observational" },
+    { emoji: "📖", value: "Story-based" },
+  ];
+
+  const [state, setState] = useState({ topic: "", tone: "", type: "" });
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) =>
+    setState({ ...state, [name]: value });
+
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+    <main className="mx-auto w-full p-24 flex flex-col">
+      <div className="p4 m-4">
+        <div className="flex flex-col items-center justify-center space-y-8 text-white">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold">Jokes Generator App</h2>
+            <p className="text-zinc-500 dark:text-zinc-400">
+              Customise the tone of the joke.
+            </p>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Topic</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {topics.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    value={value}
+                    name="topic"
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Tones</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {tones.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="tone"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Types</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {types.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="type"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+            disabled={isLoading || !state.topic || !state.tone || !state.type}
+            onClick={() =>
+              append({
+                role: "user",
+                content: `Generate a joke about ${state.topic} with ${state.tone} tone and ${state.type} type`,
+              })
+            }
+          >
+            Generate Story
+          </button>
+
+          <div
+            hidden={
+              messages.length === 0 ||
+              messages[messages.length - 1]?.content.startsWith("Generate")
+            }
+            className="bg-opacity-25 bg-gray-700 rounded-lg p-4"
+          >
+            {messages[messages.length - 1]?.content}
+          </div>
         </div>
-      ))}
- 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
+      </div>
+    </main>
   );
 }
